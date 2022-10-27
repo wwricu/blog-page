6a39fb758f014ef3946213ea166c921481b62ad5
wang.weiran <wang.wei.ran@outlook.com>
2022-10-27 22:47:57 +1100

    add login-card and login view

---
+++ src/components/cards/LoginCard.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+
+import {ref} from "vue";
+
+const loginForm = ref([
+  {
+    text: 'User Name',
+    value: '',
+    counter: 12,
+    rules: [
+      (v:String) => {
+        return !!v || 'This field is required'
+      },
+      (v:String) => {
+        return !!v && v.length <= 12
+            || 'Username must be less than 12 chars'
+      },
+    ]
+  },
+  {
+    text: 'Password',
+    value: '',
+    counter: 25,
+    rules: [
+      (v:String) => {
+        return !!v || 'This field is required'
+      },
+      (v:String) => {
+        return !!v && v.length <= 25
+            || 'Username must be less than 25 chars'
+      },
+    ]
+  }
+])
+</script>
+
+<template>
+  <v-card
+    max-width="400px"
+    max-height="600px"
+    class="bg-grey-lighten-4"
+  >
+    <v-card-text>
+      <v-text-field
+          variant="underlined"
+          color="indigo"
+          v-for="item in loginForm"
+          :key="item.text"
+          :label="item.text"
+          :counter="item.counter"
+          :rules="item.rules"
+          v-model="item.value"
+          class="ml-1 mr-1"
+      >
+      </v-text-field>
+    </v-card-text>
+    <v-divider/>
+    <v-card-actions>
+      <v-spacer/>
+      <v-btn
+        density="compact"
+        color="indigo"
+      >
+        Login
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import LoginCard from '@/components/cards/LoginCard.vue'
+</script>
+
+<template>
+  <div class="fill-height pt-16">
+    <login-card
+        class="mx-auto"
+    ></login-card>
+  </div>
+</template>
+
+<style scoped>
+div {
+  background-color: #2c3e50;
+}
+</style>
