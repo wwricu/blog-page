3f7a32cf87d3140ed9b8b3471d6d325b0dd2a10c
wang.weiran <wang.wei.ran@outlook.com>
2022-10-31 15:39:34 +1100

    add navigate-menu

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <v-layout style="z-index: 0">
-    <v-app-bar density="compact"></v-app-bar>
+    <v-app-bar density="compact">
+      <navigate-menu/>
+    </v-app-bar>
     <v-app>
       <v-main>
         <router-view/>
@@ -11,10 +13,11 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import NavigateMenu from "@/components/menus/NavigateMenu.vue";
 
 export default defineComponent({
   name: 'App',
-
+  components: {NavigateMenu},
   data () {
     return {
       //
---
+++ src/components/menus/NavigateMenu.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const Title = 'Tech'
+const subFolders = ref([
+  {
+    text: 'C++',
+    link: '/C++'
+  },
+  {
+    text: 'Java',
+    link: '/Java'
+  }
+])
+function push(link: String) {
+  alert(link)
+}
+
+</script>
+<template>
+  <v-menu transition="scroll-y-transition">
+    <template v-slot:activator="{ props }">
+      <v-btn
+          color="primary"
+          class="ma-2"
+          v-bind="props"
+      >
+        {{ Title }}
+      </v-btn>
+    </template>
+    <v-list>
+      <v-list-item
+          v-for="subFolder in subFolders"
+          :key="subFolder"
+          link
+      >
+        <v-list-item-title
+          @click="push(subFolder.text)"
+        >
+          {{subFolder.text}}
+        </v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<style scoped>
+
+</style>
