36370a8584d436b759ef7d173232f84f8ea1dda2
wang.weiran <wang.wei.ran@outlook.com>
2022-11-14 14:17:32 +1100

    add get content

--- src/apis/content.ts
+++ src/apis/content.ts
@@ -12,6 +12,32 @@ export const postContent = (data: any,
     })
 }
 
+export const getContent = (data: any,
+                           success: Function,
+                           failure: Function) => {
+    let url = '/content'
+    if (data != null) {
+        url += '?'
+        if (data.id !== undefined) {
+            url += `content_id=${data.id}&`
+        }
+        if (data.url !== undefined) {
+            url += `url=${data.url}&`
+        }
+        if (data.parent_id !== undefined) {
+            url += `parent_id=${data.parent_id}&`
+        }
+        url = url.slice(0, -1)
+    }
+
+    myAxios.request({
+        method: 'GET',
+        url: url,
+    }).then((res: any) => {
+        success(res)
+    })
+}
+
 export const getAllBlog = (success: Function,
                            failure: Function) => {
     myAxios.request({
