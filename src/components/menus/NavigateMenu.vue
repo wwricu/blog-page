ab77550044d3bdd28489f6db940d588916ce0c77
wang.weiran <wang.wei.ran@outlook.com>
2022-11-16 06:53:17 +1100

    transit when hover

--- src/components/menus/NavigateMenu.vue
+++ src/components/menus/NavigateMenu.vue
@@ -12,46 +12,48 @@ const subFolders = ref([
     link: '/Java'
   }
 ])
+
+let menu = ref()
 function push(link: String) {
   alert(link)
 }
 </script>
 
 <template>
-  <v-hover close-delay="500">
+  <v-hover close-delay="200">
     <template v-slot:default="{ isHovering, props }">
       <v-btn
           id="menu-activator"
           v-bind="props"
           rounded="pill"
           class="ma-2 pa-n2"
-          :color="isHovering ? 'white' : 'primary'"
-          :class="isHovering ? 'bg-primary' : 'bg-white'"
+          :color="isHovering || menu ? 'white' : 'primary'"
+          :class="isHovering || menu ? 'bg-primary' : 'bg-white'"
       >
         {{ Title }}
+        <v-menu
+            open-on-hover
+            activator="parent"
+            transition="scroll-y-transition"
+            v-model="menu"
+        >
+          <v-list>
+            <v-list-item
+                v-for="subFolder in subFolders"
+                :key="subFolder"
+                link
+            >
+              <v-list-item-title
+                  @click="push(subFolder.text)"
+              >
+                {{subFolder.text}}
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
       </v-btn>
     </template>
   </v-hover>
-
-  <v-menu
-      open-on-hover
-      activator="#menu-activator"
-      transition="scroll-y-transition"
-  >
-    <v-list>
-      <v-list-item
-          v-for="subFolder in subFolders"
-          :key="subFolder"
-          link
-      >
-        <v-list-item-title
-            @click="push(subFolder.text)"
-        >
-          {{subFolder.text}}
-        </v-list-item-title>
-      </v-list-item>
-    </v-list>
-  </v-menu>
 </template>
 
 <style scoped>
