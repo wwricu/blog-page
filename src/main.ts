d0eb44a6190ed2fcfaad60f48cecd29a31a7d454
wang.weiran <wang.wei.ran@outlook.com>
2022-11-27 19:26:43 +1100

    remove redundant axios config

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,10 @@ import { loadFonts } from '@/plugins/webfontloader'
 import { QuillEditor } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css';
 import { createPinia } from 'pinia'
-import axios from "axios";
 
 loadFonts().then()
 
 const app = createApp(App)
-axios.defaults.method = 'POST'
-axios.defaults.baseURL = 'http://localhost:8443'
-axios.defaults.withCredentials = true
-app.config.globalProperties.$http = axios
 
 app.component('QuillEditor', QuillEditor)
    .use(router)
