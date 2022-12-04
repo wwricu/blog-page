579b31c93a243dca0c62c790f5a1ae98ceff83a9
wang.weiran <wang.wei.ran@outlook.com>
2022-12-04 21:50:28 +1100

    use cdn for vue and vuetify

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,28 @@ import vue from '@vitejs/plugin-vue'
 
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
 import vuetify from 'vite-plugin-vuetify'
+// import importToCDN from 'vite-plugin-cdn-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
 		vue(),
 		vuetify({ autoImport: true }),
+        // importToCDN({
+        //   modules: [
+        //       {
+        //           name:"vuetify",
+        //           var:"Vuetify",
+        //           path:"https://cdn.jsdelivr.net/npm/vuetify@3.0.3/dist/vuetify.min.js",
+        //           css:"https://cdn.jsdelivr.net/npm/vuetify@3.0.3/dist/vuetify.min.css"
+        //       },
+        //   ]
+        // })
 	],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'vue': 'https://unpkg.com/vue@3/dist/vue.esm-browser.js',
     }
   }
 })
