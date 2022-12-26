d13c923f0f795f1bef12bd1cdae4aa1062131982
wang.weiran <wang.wei.ran@outlook.com>
2022-12-27 04:33:58 +1100

    import vue, vue-router and axios with cdn

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import { visualizer } from 'rollup-plugin-visualizer';
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
 import vuetify from 'vite-plugin-vuetify'
-// import importToCDN from 'vite-plugin-cdn-import'
+import importToCDN from 'vite-plugin-cdn-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,21 +17,41 @@ export default defineConfig({
           filename: 'stats.html',
           open: true
         }),
-        // importToCDN({
-        //   modules: [
-        //       {
-        //           name:"vuetify",
-        //           var:"Vuetify",
-        //           path:"https://cdn.jsdelivr.net/npm/vuetify@3.0.3/dist/vuetify.min.js",
-        //           css:"https://cdn.jsdelivr.net/npm/vuetify@3.0.3/dist/vuetify.min.css"
-        //       },
-        //   ]
-        // })
+        importToCDN({
+          modules: [
+              {
+                  name: 'axios',
+                  var: 'axios',
+                  path: 'https://cdnjs.cloudflare.com/ajax/libs/axios/1.2.2/axios.min.js'
+              },
+              {
+                  name: 'vue',
+                  var: 'Vue',
+                  path: 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.45/vue.global.min.js'
+              },
+              {
+                  name: 'vue-demi', // resolve vue var
+                  var: 'VueDemi',
+                  path: 'lib/index.iife.min.js'
+              },
+              {
+                  name: 'vue-router',
+                  var: 'VueRouter',
+                  path: 'https://cdnjs.cloudflare.com/ajax/libs/vue-router/4.1.6/vue-router.global.min.js'
+              },
+              // {
+              //     name:"vuetify",
+              //     var:"Vuetify",
+              //     path:"https://cdn.jsdelivr.net/npm/vuetify@3.0.6/dist/vuetify.min.js",
+              //     css:"https://cdn.jsdelivr.net/npm/vuetify@3.0.6/dist/vuetify.min.css"
+              // },
+          ]
+        })
 	],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'vue': 'https://unpkg.com/vue@3/dist/vue.esm-browser.js',
+      // 'vue': 'https://unpkg.com/vue@3/dist/vue.esm-browser.js',
     }
   }
 })
