979b2c5dd4bb15302a404519d36ea74f9d3660bc
wwr <me@wwr.icu>
2025-01-21 22:48:20 +0800

    [feat] change env

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,11 @@ const nextConfig: NextConfig = {
     "rc-pagination",
     "rc-input",
     "rc-picker",
-  ]
+  ],
+  env: {
+    BASE_URL: process.env.BASE_URL,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL
+  }
 };
 
 export default nextConfig;
