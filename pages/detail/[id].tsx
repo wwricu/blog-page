a0f7c8daa08d1ff395d3123beaa921cbb6ad782e
wwr <me@wwr.icu>
2025-01-23 23:35:29 +0800

    [feat] update detail page

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,6 +1,7 @@
 import {PostDetailVO} from "@/common/model";
 import {GetPostDetailAPI} from "@/common/api";
 import {GetServerSideProps, InferGetServerSidePropsType} from "next";
+import {Flex} from "antd";
 
 
 export const getServerSideProps = (async (context) => {
@@ -13,11 +14,19 @@ export const getServerSideProps = (async (context) => {
 
 export default function PostDetailPage({ postDetailVO }: InferGetServerSidePropsType<typeof getServerSideProps>) {
     return (
-        <>
-            <div>
-                <p>{postDetailVO.title}</p>
+        <Flex
+            justify='center'
+            style={{
+            backgroundImage: 'url("https://www.toptal.com/designers/subtlepatterns/uploads/leaves.png")',
+            height: '100vh',
+            width: '100%',
+        }}>
+            <div style={{width: '600px'}}>
+                <div>
+                    <p>{postDetailVO.title}</p>
+                </div>
+                <div dangerouslySetInnerHTML={{__html: postDetailVO.content}} ></div>
             </div>
-            <div dangerouslySetInnerHTML={{__html: postDetailVO.content}} ></div>
-        </>
+        </Flex>
     )
 }
