e1baa548e78ea61af7aa0656b61b5e141df7ae01
wwr <me@wwr.icu>
2025-01-30 20:08:37 +0800

    [feat] taglist ssr

--- components/TagList.tsx
+++ components/TagList.tsx
@@ -1,19 +1,13 @@
-import {useEffect, useState} from "react";
-import {GetAllTagsAPI} from "@/common/api";
-import {TagTypeEnum, TagVO} from "@/common/model";
+import {TagVO} from "@/common/model";
 
+type TagListProps = {
+    tags: TagVO[]
+}
 
-export default function TagList() {
-    const [tagList, setTagList] = useState<TagVO[]>([])
-    useEffect(() => {
-        GetAllTagsAPI(TagTypeEnum.POST_CAT).then(res => {
-            console.log(res)
-            setTagList(res)
-        })
-    }, [])
+export default function TagList({tags}: TagListProps) {
     return (
         <>
-            {tagList.map((tag: TagVO) => (
+            {tags.map((tag: TagVO) => (
                 <p key={tag.id}>{tag.name}</p>
             ))}
         </>
--- pages/category/index.tsx
+++ pages/category/index.tsx
@@ -1,6 +1,14 @@
 import TagList from "@/components/TagList";
+import {TagTypeEnum, TagVO} from "@/common/model";
+import {GetAllTagsAPI} from "@/common/api";
+import {GetServerSideProps, InferGetServerSidePropsType} from "next";
 
 
-export default function category() {
-    return <TagList/>
+export const getServerSideProps = (async () => {
+    const tags: TagVO[] = await GetAllTagsAPI(TagTypeEnum.POST_CAT)
+    return { props: { tags } }
+}) satisfies GetServerSideProps<{ tags: TagVO[] }>
+
+export default function category({ tags }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+    return <TagList tags={tags}/>
 }
