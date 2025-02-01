d151449e906eaf9ed956dd27e57f86e9965b4daf
wwr <me@wwr.icu>
2025-02-01 22:29:13 +0800

    [feat] add about

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,43 +1,25 @@
 import React from 'react';
-import {AppstoreOutlined, HomeOutlined, TagsOutlined} from '@ant-design/icons';
-import type { MenuProps } from 'antd';
+import {AppstoreOutlined, HomeOutlined, InfoCircleOutlined, TagsOutlined} from '@ant-design/icons';
 import { Menu } from 'antd';
 import Link from "next/link";
 
-type MenuItem = Required<MenuProps>['items'][number];
 
-const items: MenuItem[] = [
-    {
-        label: (
-            <Link href="/" rel="noopener noreferrer">
-                Home
-            </Link>
-        ),
-        key: 'home',
-        icon: <HomeOutlined/>,
-    },
-    {
-        label: (
-            <Link href="/category" rel="noopener noreferrer">
-                Category
-            </Link>
-        ),
-        key: 'category',
-        icon: <AppstoreOutlined/>,
-    },
-    {
-        label: (
-            <Link href="/tag" rel="noopener noreferrer">
-                Tags
-            </Link>
-        ),
-        key: 'tags',
-        icon: <TagsOutlined/>,
-    },
-];
-// f6f8fa 246 248 250
 const Header: React.FC = () => {
-    return <Menu mode="horizontal" items={items} />;
+    // TODO: display: block make diff on menu line height
+    return <Menu className='leading-10 block' mode="horizontal">
+        <Menu.Item key="home" icon={(<HomeOutlined/>)}>
+            <Link href="/" rel="noopener noreferrer">Home</Link>
+        </Menu.Item>
+        <Menu.Item key="categories" icon={(<AppstoreOutlined/>)}>
+            <Link href="/category" rel="noopener noreferrer">Category</Link>
+        </Menu.Item>
+        <Menu.Item key="tags" icon={(<TagsOutlined/>)}>
+            <Link href="/tag" rel="noopener noreferrer">Tags</Link>
+        </Menu.Item>
+        <Menu.Item className='float-right' key="about" icon={(<InfoCircleOutlined/>)}>
+            <Link href="/about" rel="noopener noreferrer">About</Link>
+        </Menu.Item>
+    </Menu>;
 };
 
 export default Header;
---
+++ pages/about/index.tsx
@@ -0,0 +1,5 @@
+export default () => {
+    return (
+        <p>On going...</p>
+    )
+}
