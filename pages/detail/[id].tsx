ccb2e1408ab9e956bad771d3f170b220e60d1308
wwr <me@wwr.icu>
2025-02-09 21:17:35 +0800

    [feat] render title, time, cat and tags

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,16 +1,46 @@
-import {PostDetailVO} from "@/common/model";
+import {PostDetailVO, TagVO} from "@/common/model";
 import {GetPostDetailAPI} from "@/common/api";
 import {GetServerSideProps, InferGetServerSidePropsType} from "next";
-import {Flex} from "antd";
+import {Flex, Typography} from "antd";
 import React from "react";
+import {AppstoreOutlined, ClockCircleOutlined, TagsOutlined} from "@ant-design/icons";
 
 
 export const getServerSideProps = (async (context) => {
     const { params } = context;
     const postId = params!.id;
     const postDetailVO = await GetPostDetailAPI(postId as string)
+    postDetailVO.tag_list = [{id: 1, name: 'tag1', type: 'tag'}, {id: 2, name: 'tag2', type: 'tag'}];
+    postDetailVO.category = {id: 1, name: 'category', type: 'tag'}
     return { props: { postDetailVO } }
 }) satisfies GetServerSideProps<{ postDetailVO: PostDetailVO }>
+
+
+const renderTags = (postDetailVO: PostDetailVO) => {
+    if (postDetailVO?.tag_list == null || postDetailVO?.tag_list.length === 0) {
+        return <></>
+    }
+    return <Typography.Text type='secondary'>
+        <TagsOutlined className='mr-1'/>
+        {postDetailVO?.tag_list.map((tag: TagVO) => (
+            <Typography.Link key={tag.id} href={`/tags/${tag.name}`} className='text-base mx-1'>
+                {tag.name}
+            </Typography.Link>
+        ))}
+    </Typography.Text>
+}
+
+const renderCategory = (postDetailVO: PostDetailVO) => {
+    if (postDetailVO?.category == null) {
+        return <></>
+    }
+    return <Typography.Text type='secondary'>
+        <AppstoreOutlined className='mr-1'/>
+        <Typography.Link href={`/categories/${postDetailVO?.category.name}`} className='text-base mx-1'>
+            {postDetailVO?.category?.name}
+        </Typography.Link>
+    </Typography.Text>
+}
 
 
 export default function PostDetailPage({ postDetailVO }: InferGetServerSidePropsType<typeof getServerSideProps>) {
@@ -20,10 +50,18 @@ export default function PostDetailPage({ postDetailVO }: InferGetServerSideProps
             justify='center'
         >
             <div className='w-800px border-x p-4'>
-                <div>
-                    <p>{postDetailVO.title}</p>
-                </div>
-                <div dangerouslySetInnerHTML={{__html: postDetailVO.content}} ></div>
+                <Typography.Title level={3}>{postDetailVO.title}</Typography.Title>
+                <Flex gap='small' className='my-6' align='baseline'>
+                    <Typography.Text type='secondary'>
+                        <ClockCircleOutlined className='mr-1'/>
+                        {postDetailVO.create_time?.slice(0, 10)}
+                    </Typography.Text>
+                    {renderCategory(postDetailVO)}
+                    {renderTags(postDetailVO)}
+                </Flex>
+                <Typography.Paragraph>
+                    <div dangerouslySetInnerHTML={{__html: postDetailVO.content}} ></div>
+                </Typography.Paragraph>
             </div>
         </Flex>
     )
