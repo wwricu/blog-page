58c7eebbb982744ef39a53f3cca200f91b87573f
wwr <me@wwr.icu>
2025-02-11 21:17:39 +0800

    [feat] update index style

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -24,12 +24,12 @@ export default function PostList({postDetailPageVO}: PostListProps) {
     }
 
     return (
-        <Flex className='grow' justify="center">
+        <Flex className='grow my-2' justify="center">
             <Flex className='w-800px' vertical>
                 {postDetailPageVO?.post_details?.map(postDetailVO =>
                     <PostCard key={postDetailVO.id} postDetailVO={postDetailVO}/>)}
                 <Pagination
-                    className='mt-2'
+                    className='my-2'
                     align="center"
                     current={postDetailPageVO.page_index}
                     total={postDetailPageVO.count}
--- components/TagList.tsx
+++ components/TagList.tsx
@@ -9,18 +9,15 @@ type TagListProps = {
 // TODO: 1. Add post count 2. Add description
 export default function TagList({tags, type}: TagListProps) {
     const path = type === TagTypeEnum.POST_TAG ? '/tags/' : '/categories/';
-    return (
-        <Row justify='center'>
-            <Flex className='mt-3 w-800px'>
-                    {tags.map((tag: TagVO) => (
-                        <Typography.Link key={tag.id} href={path + tag.name}>
-                            <div className='bg-white shadow-sm mx-2 my-1 min-w-24 text-lg text-center w-20 border rounded-lg'>
-                                {tag.name}
-                            </div>
-                        </Typography.Link>
-                    ))}
-            </Flex>
-        </Row>
-
-    )
+    return <Row justify='center'>
+        <Flex className='mt-3 w-800px'>
+                {tags.map((tag: TagVO) => (
+                    <Typography.Link key={tag.id} href={path + tag.name}>
+                        <div className='bg-white shadow-sm mx-2 my-1 min-w-24 text-lg text-center w-20 border rounded-lg'>
+                            {tag.name}
+                        </div>
+                    </Typography.Link>
+                ))}
+        </Flex>
+    </Row>
 }
