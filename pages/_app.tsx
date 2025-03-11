522ef8bcff8165806a0575b459f6b987e16c814a
wwr <me@wwr.icu>
2025-03-12 00:15:29 +0800

    [feat] dynamic year in header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ export default function App({Component, pageProps}: AppProps) {
             </Content>
             <Footer className='shadow-md text-center bg-transparent mt-0 pt-0 sm:h-12 max-sm:h-6'>
                     <Divider className='mt-0 mb-3 max-sm:hidden'/>
-                    wwr.icu 2025
+                    wwr.icu {new Date().getFullYear()}
             </Footer>
         </Layout>
     )
