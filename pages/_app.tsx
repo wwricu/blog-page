5684cd0d1297abf52661775b7b0333d39f3216a9
wwr <me@wwr.icu>
2026-01-26 01:31:50 +0800

    [transfer] remove Ant design Layout, Content, Footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,20 +2,17 @@ import "@/styles/globals.css"
 import type {AppProps} from "next/app"
 import Header from "@/components/Header";
 import React from "react";
-import {Divider, Layout} from "antd";
-import {Content, Footer} from "antd/es/layout/layout";
+import {Divider} from "antd";
 
 export default function App({Component, pageProps}: AppProps) {
     return (
-        <Layout className='min-h-lvh bg-slate-100 bg-[url(/dust_scratches.webp)]'>
-            <Content >
-                <Header/>
-                <Component {...pageProps} />
-            </Content>
-            <Footer className='shadow-md text-center bg-transparent mt-0 pt-0 sm:h-12 max-sm:h-6'>
+        <div className='min-h-lvh bg-slate-100 bg-[url(/dust_scratches.webp)]'>
+            <Header/>
+            <Component {...pageProps} />
+            <div className='shadow-md text-center bg-transparent mt-0 pt-0 sm:h-12 max-sm:h-6'>
                 <Divider className='mt-0 mb-3 max-sm:hidden'/>
                 wwr.icu {new Date().getFullYear()}
-            </Footer>
-        </Layout>
+            </div>
+        </div>
     )
 }
