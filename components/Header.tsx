95e04f25d9d6b4cc6858d33958e45dbaef9238f8
wwr <me@wwr.icu>
2026-01-26 15:03:04 +0800

    [feat] ant design Button to daisyUI

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ import {
     MailOutlined,
     TagsOutlined
 } from '@ant-design/icons';
-import {Button, Modal, Statistic} from 'antd';
+import {Modal, Statistic} from 'antd';
 import {GetAboutAPI} from "@/common/api";
 import {useRouter} from "next/router";
 import Link from "next/link";
@@ -21,7 +21,7 @@ const Header: React.FC = () => {
     const router = useRouter()
 
     const statisticClassName = 'text-sm text-center'
-    const menuItemClassName = 'h-full rounded-none border-0 shadow-none max-sm:px-2 text-text-prime'
+    const menuItemClassName = 'btn btn-sm h-full font-light rounded-none border-0 shadow-none max-sm:px-2 text-text-prime'
 
     useEffect(() => {
         GetAboutAPI().then((res) => {
@@ -46,27 +46,27 @@ const Header: React.FC = () => {
             <div className='flex justify-between items-center sm:h-10 bg-transparent flex-wrap border-solid border-b-2'>
                 <div className='flex justify-start items-center flex-wrap h-full'>
                     <Link href='/' className='h-full'>
-                        <Button className={menuItemClassName + getButtonStyle('/')}>
+                        <button className={menuItemClassName + getButtonStyle('/')}>
                             <HomeOutlined/>Home
-                        </Button>
+                        </button>
                     </Link>
                     <Link href='/categories' className='h-full'>
-                        <Button className={menuItemClassName + getButtonStyle('/categories')}>
+                        <button className={menuItemClassName + getButtonStyle('/categories')}>
                             <AppstoreOutlined/>Category
-                        </Button>
+                        </button>
                     </Link>
                     <Link href='/tags' className='h-full'>
-                        <Button onClick={() => router.push('/tags')} className={menuItemClassName + getButtonStyle('/tags')}>
+                        <button onClick={() => router.push('/tags')} className={menuItemClassName + getButtonStyle('/tags')}>
                             <TagsOutlined/>Tags
-                        </Button>
+                        </button>
                     </Link>
                 </div>
-                <Button onClick={() => setIsModalOpen(true)} className={menuItemClassName + ' hover:!bg-slate-300 hover:!text-text-prime ' + (isModalOpen ? ' bg-indigo-600 text-white' : 'bg-transparent')}>
+                <button onClick={() => setIsModalOpen(true)} className={menuItemClassName + ' hover:!bg-slate-300 hover:!text-text-prime ' + (isModalOpen ? ' bg-indigo-600 text-white' : 'bg-transparent')}>
                     <InfoCircleOutlined/>
                     <span className='max-sm:hidden'>
                     About
                 </span>
-                </Button>
+                </button>
             </div>
             <Modal
                 footer={null}
@@ -84,8 +84,14 @@ const Header: React.FC = () => {
                 </div>
                 <div className='divider my-4'/>
                 <div className='flex justify-between'>
-                    <Button type='primary' href='https://github.com/wwricu' target='_blank' className='m-2 grow'><GithubOutlined/>GitHub</Button>
-                    <Button href='mailto:me@wwr.icu' className='m-2 grow'><MailOutlined/>Mail me</Button>
+                    <Link href='https://github.com/wwricu'>
+                        <button className='btn btn-active btn-neutral btn-sm m-2 rounded grow'><GithubOutlined/>GitHub</button>
+                    </Link>
+                    <Link href='mailto:me@wwr.icu'>
+                        <button className='btn bg-white text-black border-[#e5e5e5] rounded-s btn-sm rounded m-2 grow'>
+                            <MailOutlined/>Mail me
+                        </button>
+                    </Link>
                 </div>
             </Modal>
         </>
