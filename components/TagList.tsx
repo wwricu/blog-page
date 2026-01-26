d50eb1e313703c604c0f3150b07a416e4eb8162f
wwr <me@wwr.icu>
2026-01-26 15:59:43 +0800

    [fix] fix footer

--- components/TagList.tsx
+++ components/TagList.tsx
@@ -1,5 +1,4 @@
 import {TagVO} from "@/common/model";
-import {Tag} from "antd";
 import Link from "next/link";
 
 type TagListProps = {
@@ -12,9 +11,7 @@ export default function TagList({tags}: TagListProps) {
             <div className='flex mt-3 w-md flex-wrap'>
                 {tags.map((tag: TagVO) => (
                     <Link key={tag.id} href={`/tags/${tag.name}`}>
-                        <Tag
-                            className='bg-[rgba(255,255,255,0.3)] text-sm text-text-prime shadow-sm m-1 px-2 min-w-[50px] max-w-[100vw] border rounded-lg hover:shadow-md hover:bg-slate-100'
-                        >
+                        <div className='badge badge-sm badge-neutral badge-outline text-sm text-text-prime shadow-sm m-1 px-2 min-w-[50px] max-w-[100vw] border rounded-lg hover:shadow-md hover:bg-slate-100'>
                             <div className='justify-between items-baseline gap-2'>
                                 <span className='overflow-hidden'>
                                     {tag.name}
@@ -23,7 +20,7 @@ export default function TagList({tags}: TagListProps) {
                                     {tag.count}
                                 </span>
                             </div>
-                        </Tag>
+                        </div>
                     </Link>
                 ))}
             </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,13 @@ import React from "react";
 
 export default function App({Component, pageProps}: AppProps) {
     return (
-        <div className='min-h-lvh bg-slate-100 bg-[url(/dust_scratches.webp)]'>
-            <Header/>
-            <Component {...pageProps} />
-            <div className='shadow-md text-center bg-transparent mt-0 pt-0 sm:h-12 max-sm:h-6'>
-                <div className='divider mt-0 mb-3 max-sm:hidden'/>
+        <div className='flex flex-col min-h-screen bg-slate-100 bg-[url(/dust_scratches.webp)]'>
+            <main className='flex-1'>
+                <Header/>
+                <Component {...pageProps} />
+            </main>
+            <div className='text-gray-500 text-center bg-transparent sm:h-12 max-sm:h-6'>
+                <div className='divider mt-0 mb-0 max-sm:hidden'/>
                 wwr.icu {new Date().getFullYear()}
             </div>
         </div>
