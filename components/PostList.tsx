7f13aaa77ccd9ea4825f8a137d03bb56fc8da785
wwr <me@wwr.icu>
2026-01-27 12:43:04 +0800

    [feat] add Pagination

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -2,7 +2,7 @@ import {PostDetailPageVO} from "@/common/model";
 import PostCard from "@/components/PostCard";
 import React, {useEffect} from "react";
 import {useRouter} from "next/router";
-import Link from "next/link";
+import Pagination from "@/components/Pagination";
 
 type PostListProps = {
     postDetailPageVO: PostDetailPageVO;
@@ -16,8 +16,8 @@ export default function PostList({postDetailPageVO}: PostListProps) {
     * Since we use itemRender to assign page url to each button,
     * onChange is not reliable anymore because it is invoked on click before goto new page by url on buttons.
     * Next.js does not refresh the entire page under the same router so the page number behaves correctly
-    * when user jump between pages while the page was fully loaded when user click page button on the index page
-    * that router changes, so I suggest to use useEffect hoot to ensure everyone sees correct page number.
+    * when user jump between pages but the page was fully loaded when user click page button on the index page
+    * that router changes, so I suggest to use useEffect hook to ensure everyone sees correct page number.
     */
     useEffect(() => {
         setCurrent(postDetailPageVO.page_index)
@@ -32,20 +32,16 @@ export default function PostList({postDetailPageVO}: PostListProps) {
         return `${pathname}${split}${page}`
     }
 
-    const itemRender = (
-        page: number,
-        type: 'page' | 'prev' | 'next' | 'jump-prev' | 'jump-next',
-        element: React.ReactNode
-    ) => {
-        return <Link href={getUrl(page)}>{type === 'page' ? page : element}</Link>
-    }
-    void itemRender
-    void current
-
     return (
         <div className='flex flex-col items-center'>
             {postDetailPageVO?.data?.map(postDetailVO =>
                 <PostCard key={postDetailVO.id} postDetailVO={postDetailVO}/>)}
+            <Pagination
+                className='mt-4 mb-2'
+                current={current}
+                total={postDetailPageVO.count}
+                getHref={getUrl}
+            />
         </div>
     )
 }
