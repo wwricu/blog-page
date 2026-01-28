9a577bf5bf1903e2f8968c0ccf84a96ae9a38b7f
wwr <me@wwr.icu>
2026-01-28 12:32:06 +0800

    tmp

--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -1,16 +1,16 @@
-import {PostDetailVO, TagVO} from "@/common/model";
-import {GetPostDetailAPI} from "@/common/api";
-import React from "react";
+import {PostDetailVO, TagVO} from "@/common/model"
+import {GetPostDetailAPI} from "@/common/api"
+import React from "react"
 import {Clock, Hash, Tags} from 'lucide-react'
-import {AsyncPathParams, getTagColorClass} from "@/common/common";
+import {AsyncPathParams, getTagColorClass} from "@/common/common"
 
 const renderTags = (postDetailVO: PostDetailVO) => {
     if (postDetailVO?.tag_list == null || postDetailVO?.tag_list.length === 0) {
         return <></>
     }
     return (
-        <div className='flex justify-start flex-wrap'>
-            <Tags className='inline text-gray-500 mr-1' size={16} color="#757575" strokeWidth={2}/>
+        <span className='flex-wrap'>
+            <Tags className='inline text-gray-600 mr-1' size={16} color="#757575" strokeWidth={2}/>
             {postDetailVO?.tag_list.map((tag: TagVO) => (
                 <a key={tag.id} href={`/tags/${tag.name}`} className='ml-1'>
                     <div className={'badge badge-sm rounded' + getTagColorClass()}>
@@ -18,7 +18,7 @@ const renderTags = (postDetailVO: PostDetailVO) => {
                     </div>
                 </a>
             ))}
-        </div>
+        </span>
     )
 }
 
@@ -27,30 +27,30 @@ const renderCategory = (postDetailVO: PostDetailVO) => {
         return <></>
     }
     return (
-        <div className='text-gray-500'>
-            <Hash className='inline mr-2' size={16} color="#757575" strokeWidth={2}/>
-            <a href={`/categories/${postDetailVO?.category.name}`}>
+        <span>
+            <Hash className='inline mr-1' size={16} color="#757575" strokeWidth={2}/>
+            <a className='text-gray-600' href={`/categories/${postDetailVO?.category.name}`}>
                 <div className='badge badge-neutral badge-sm badge-outline rounded bg-white text-gray-400'>
                     {postDetailVO?.category?.name}
                 </div>
             </a>
             <div className='divider-horizontal'/>
-        </div>
+        </span>
     )
 }
 
 export default async function PostDetailPage({ params }: AsyncPathParams) {
-    const { id } = await params;
+    const { id } = await params
     const postDetailVO = await GetPostDetailAPI(id!!)
     return (
         <div className='flex justify-center min-h-lvh w-full '>
             <div className='w-3xl bg-[rgba(240,240,240,0.5)] shadow-sm border-x border-gray-200 p-4'>
                 <h1 className='text-2xl font-semibold'>{postDetailVO.title}</h1>
                 <div className='flex items-center gap-2 mt-6 mb-8 flex-wrap'>
-                    <p className='text-gray-500'>
+                    <span>
                         <Clock className='inline mr-1' size={16} color="#757575" strokeWidth={2}/>
-                        {postDetailVO.create_time?.slice(0, 10)}
-                    </p>
+                        <span className='text-gray-600 align-[-1]'>{postDetailVO.create_time?.slice(0, 10)}</span>
+                    </span>
                     <div className='divider-horizontal'/>
                     {renderCategory(postDetailVO)}
                     {renderTags(postDetailVO)}
