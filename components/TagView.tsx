5e0d32d1011c1d42cfbda2cd9eff1e8971c9d232
wwr <me@wwr.icu>
2026-02-13 00:57:32 +0800

    [fix] fix tag view

--- components/TagView.tsx
+++ components/TagView.tsx
@@ -2,9 +2,10 @@ import {TagTypeEnum, TagVO} from "@/common/model"
 import Link from "next/link"
 import React from "react";
 import {GetAllTagsAPI} from "@/common/api"
+import {CategoriesURL, TagsUrl} from "@/common/common"
 
 export default async function TagView({ tagType }: {  tagType: TagTypeEnum }) {
-    const tags: TagVO[] = await GetAllTagsAPI(TagTypeEnum.POST_CAT)
+    const tags: TagVO[] = await GetAllTagsAPI(tagType)
     return (
         <div className='flex flex-col flex-1 overflow-x-auto w-full gap-6 bg-base-100 shadow-sm max-sm:p-4 sm:p-6 max-md:w-full md:w-3xl'>
         {
@@ -12,7 +13,7 @@ export default async function TagView({ tagType }: {  tagType: TagTypeEnum }) {
                 <div key={tag.id} className='flex justify-between items-baseline min-w-2xs'>
                     <Link
                         className='link text-base-content/80 transition-colors hover:text-primary'
-                        href={`/${tagType === TagTypeEnum.POST_CAT ? 'categories' : 'tags'}/${tag.name}`}
+                        href={`/${tagType === TagTypeEnum.POST_CAT ? CategoriesURL : TagsUrl}/${tag.name}`}
                     >
                         {tag.name}
                     </Link>
