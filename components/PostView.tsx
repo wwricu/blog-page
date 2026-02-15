70a01f2e511341a64ea10a5f9507da776c283654
wwr <me@wwr.icu>
2026-02-15 21:16:56 +0800

    [feat] extract pagination

--- components/PostView.tsx
+++ components/PostView.tsx
@@ -4,101 +4,7 @@ import React from "react"
 import {GetAllBlogPosts} from "@/common/api"
 import {CategoriesURL, PathParams, TagsUrl} from "@/common/common"
 import NotFound from "next/dist/client/components/builtin/not-found"
-import Link from "next/link"
-
-type PaginationProps = {
-    total: number,
-    current: number
-    pageSize?: number
-    className?: string
-    baseUrl: string
-}
-
-const Pagination = ({ current = 1, total, pageSize = 10, baseUrl, className }: PaginationProps) => {
-    const delta = 1
-    const pageCount = Math.ceil(total / pageSize)
-    if (total <= 0 || current <= 0 || current > pageCount) {
-        return null
-    }
-
-    const getButtonValues = () => {
-        const pages: (number | string)[] = []
-        if (pageCount < 1) {
-            return pages
-        }
-
-        pages.push(1)
-
-        if (current - 1 > delta + 1) {
-            pages.push('...')
-        }
-
-        for (let i = 1; i <= delta; i++) {
-            if (current - delta + i - 1 > 1) {
-                pages.push(current - delta + i - 1)
-            }
-        }
-
-        if (current > 1 && current < pageCount) {
-            pages.push(current)
-        }
-
-        for (let i = 1; i <= delta; i++) {
-            if (current + i < pageCount) {
-                pages.push(current + i)
-            }
-        }
-
-        if (current + delta < pageCount - 1) {
-            pages.push('...')
-        }
-
-        if (pageCount > 1) {
-            pages.push(pageCount)
-        }
-
-        return pages
-    }
-
-    const getUrl = (page: string) => {
-        if (page === '1') {
-            return baseUrl
-        }
-        // split for unexpected tailing slash
-        return `${baseUrl}${baseUrl.endsWith('/') ? '' : '/'}${page}`
-    }
-
-    return (
-        <div className={`join ${className}`}>
-            {
-                getButtonValues().map((value, i) => {
-                    const status = value === current ? 'btn-active btn-primary text-primary-content' : 'btn-ghost'
-                    return (
-                        <Link
-                            key={i}
-                            href={value === '...' ? '' : getUrl(value.toString())}
-                            className={`join-item btn btn-outline border-base-300 text-base-content max-md:btn-sm md:btn-md ${status}`}
-                        >
-                            {value}
-                        </Link>
-                    )
-                })
-            }
-        </div>
-    )
-}
-
-const BreadCrumb = async ({ filter, name, className }: PathParams)=> {
-    return (
-        <div className={`breadcrumbs text-base-content/70 text-sm w-full ${className}`}>
-            <ul>
-                <li><a className='hover:text-primary' href={'/'}>Home</a></li>
-                <li><a className='hover:text-primary' href={`/${filter}`}>{filter}</a></li>
-                { name ? <li>{decodeURIComponent(name)}</li> : null }
-            </ul>
-        </div>
-    )
-}
+import Pagination from "@/components/Pagination"
 
 export default async function PostView({ filter, name, index = '1' }: PathParams) {
     const apiParams: { index: number, category?: string, tag?: string } = { index: Number(index) }
@@ -131,7 +37,15 @@ export default async function PostView({ filter, name, index = '1' }: PathParams
         }>
             {
                 (filter === CategoriesURL || filter === TagsUrl) && name ?
-                <BreadCrumb className='py-0 min-w-2xs max-sm:pl-2 sm:pl-4 max-md:w-full md:w-3xl' filter={filter} name={name}/> : null
+                (
+                    <div className={`breadcrumbs py-0 min-w-2xs text-base-content/70 text-sm w-full max-sm:pl-2 sm:pl-4 max-md:w-full md:w-3xl`}>
+                        <ul>
+                            <li><a className='hover:text-primary' href={'/'}>Home</a></li>
+                            <li><a className='hover:text-primary' href={`/${filter}`}>{filter}</a></li>
+                            { name ? <li>{decodeURIComponent(name)}</li> : null }
+                        </ul>
+                    </div>
+                ) : null
             }
             {
                 postDetailPageVO?.data?.map((postDetailVO, i) =>
