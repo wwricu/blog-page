1ec34f101ea2346d216ff7d6aaeac51fe595989e
wwr <me@wwr.icu>
2026-04-28 04:24:35 +0800

    add bundle analyser

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
+import withBundleAnalyzer from '@next/bundle-analyzer'
 
 const nextConfig: NextConfig = {
   output: 'standalone',
@@ -13,6 +14,6 @@ const nextConfig: NextConfig = {
       },
     ],
   }
-};
+}
 
-export default nextConfig;
+export default withBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' })(nextConfig)
