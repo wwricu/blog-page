67cb43aed598c2e35c646fc18836951fdfe5cc08
wwr <me@wwr.icu>
2026-05-04 13:56:12 +0800

    minimize font

--- components/TagView.tsx
+++ components/TagView.tsx
@@ -7,12 +7,12 @@ import {CategoriesURL, TagsUrl} from "@/common/common"
 export default async function TagView({ tagType }: {  tagType: TagTypeEnum }) {
     const tags: TagVO[] = await GetAllTagsAPI(tagType)
     return (
-        <div className='flex flex-col flex-1 overflow-x-auto w-full gap-6 bg-base-100 shadow-sm max-sm:p-4 sm:p-6 max-md:w-full md:w-3xl'>
+        <div className='flex flex-col flex-1 overflow-x-auto w-full gap-4 bg-base-100 shadow-sm max-sm:p-4 sm:p-6 max-md:w-full md:w-3xl'>
         {
             tags.map((tag: TagVO) => (
                 <div key={tag.id} className='flex justify-between items-baseline min-w-2xs'>
                     <Link
-                        className='link text-base-content/80 transition-colors hover:text-primary'
+                        className='link text-base-content/80 transition-colors hover:text-primary text-sm'
                         href={`/${tagType === TagTypeEnum.POST_CAT ? CategoriesURL : TagsUrl}/${encodeURIComponent(tag.name)}`}
                     >
                         {tag.name}
