6471038a6d7ffddf30a1a0334e688ac21c822558
wwr <me@wwr.icu>
2026-05-04 12:18:43 +0800

    update post card

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -8,16 +8,16 @@ export default function PostCard({ postDetailVO, index, className }: { postDetai
     const hasCover = postDetailVO.cover?.url != null && postDetailVO.cover?.name != null
     return (
         <div className={
-            `flex justify-between p-0 min-w-2xs bg-base-100 ${className} relative hover:shadow-lg transition hover:-translate-y-px
-            max-sm:min-h-16 max-sm:rounded-lg sm:rounded-xl max-md:w-full max-md:min-h-20
-            md:rounded-box md:w-3xl ${hasCover ? 'md:h-45' : 'md:min-h-45'}`
+            `${className} flex justify-between px-1 min-w-2xs bg-base-100 rounded-lg
+            relative hover:shadow-lg transition hover:-translate-y-px
+            max-sm:min-h-16 max-md:w-full md:w-3xl ${hasCover ? 'md:h-36' : 'md:min-h-36'}`
         }>
             <div className={`flex flex-col flex-1 items-start min-w-0 max-sm:p-2 sm:p-4`}>
                 <Link className='w-full' href={`/detail/${postDetailVO.id}`}>
-                    <h4 className='text-base-content font-medium mb-1 line-clamp-1 transition-colors hover:text-primary sm:text-lg md:text-xl'>
+                    <h4 className='text-base-content font-medium line-clamp-1 transition-colors hover:text-primary sm:text-lg'>
                         {postDetailVO.title}
                     </h4>
-                    <p className={`text-base-content/70 line-clamp-2 font-normal max-h-16 min-h-0 ${hasCover ? 'mt-4' : 'my-4'} max-sm:text-xs max-md:my-2`}>
+                    <p className={`text-base-content/70 line-clamp-2 font-normal max-h-16 min-h-0 text-sm ${hasCover ? 'mt-2' : 'my-2'} max-sm:text-xs`}>
                         {postDetailVO.preview}
                     </p>
                 </Link>
@@ -31,15 +31,15 @@ export default function PostCard({ postDetailVO, index, className }: { postDetai
             </div>
             {
                 hasCover ? (
-                    <Link className="relative w-60 h-45 max-md:hidden" href={`/detail/${postDetailVO.id}`}>
+                    <Link className="relative w-48 h-36 max-md:hidden" href={`/detail/${postDetailVO.id}`}>
                         <Image
                             // TODO: WHY preload & priority not work
                             priority={ index < 3 }
                             loading={ index < 3 ? 'eager' : 'lazy' }
                             fetchPriority={ index < 3 ? 'high' : 'auto' }
                             quality={75}
-                            width={240}
-                            height={180}
+                            width={192}
+                            height={144}
                             alt={postDetailVO.cover!.name!}
                             src={postDetailVO.cover!.url}
                             className='object-cover rounded-r-box h-full'
