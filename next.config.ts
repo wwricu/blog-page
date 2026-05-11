c4f8c0e06fe2e68084146be6f03d41110ff4f56d
wwr <me@wwr.icu>
2026-05-12 04:24:56 +0800

    add domain image

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,12 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 's3.**.amazonaws.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
         hostname: '**.wwr.icu',
         port: '',
         pathname: '**',
